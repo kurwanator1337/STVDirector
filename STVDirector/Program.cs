1db6e0eeed73d8f76c37e9dcbb6c21d3c97d36b2
SGKABARDIN <sgkabardin@yandex.ru>
2024-07-21 15:17:02 +0400

    Подключение к хабу вынесено в конфиг

--- STVDirector/Program.cs
+++ STVDirector/Program.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using STVDirector.Properties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace STVDirector
 {
@@ -14,16 +13,15 @@ namespace STVDirector
         private static int matchID = 0;
         private static TimeZoneInfo moscowTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
         private static HubConnection hubConnection;
+        private static bool matchEnded = false;
+        private static bool quit = false;
 
         static async Task Main(string[] args)
         {
-            ClientMod.SendCommandToWindow($"echo хуй");
-            ClientMod.SendCommandToWindow($"echo dick");
-
             try
             {
                 hubConnection = new HubConnectionBuilder()
-                    .WithUrl(new Uri($"http://localhost:4001/STVDirectorHub"))
+                    .WithUrl(new Uri(Resources.HubAddress))
                     .Build();
 
                 hubConnection.On("GetMessage", (string message) =>
@@ -38,13 +36,24 @@ namespace STVDirector
 
                 hubConnection.On("GetMatchLogs", (List<MatchLog> logs) =>
                 {
-                    if (logs.Where(x => x.Message.Contains("<Match End>")).Any())
+                    if (matchEnded)
                     {
-                        Task.Run(async () =>
+                        if (quit)
                         {
-                            await Task.Delay(70000);
-                            Environment.Exit(0);
-                        });
+                            Task.Run(async () =>
+                            {
+                                Console.WriteLine("матч завершился!");
+
+                                quit = false;
+
+                                await Task.Delay(70000);
+
+                                if (matchEnded)
+                                {
+                                    Environment.Exit(0);
+                                }
+                            });
+                        }
                     }
 
                     DateTime criteria = DateTime.Now.AddSeconds(-61);
@@ -54,14 +63,18 @@ namespace STVDirector
 
                     if (msg != null)
                     {
+                        matchEnded = false;
                         var match = Regex.Match(msg.Message, @"(.*?)<(.+?)>\s+killed\s+(.*?)<(.+?)>\s+with\s+weapon\s+<(.*?)>\s+(\<.+?\>) \[([\d:.]+)\]$");
                         if (match.Success)
                         {
                             string name = match.Groups[1].Value;
                             Console.WriteLine($"переключаемся на {name}");
                             ClientMod.SendCommandToWindow($"spec_player \"{name}\"");
-                            ClientMod.SendCommandToWindow($"say \"{name}\"");
                         }
+                    }
+                    else
+                    {
+                        matchEnded = true;
                     }
                 });
 
